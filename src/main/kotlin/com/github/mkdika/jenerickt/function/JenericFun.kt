9e189305a9395634bf170ca92c6106b2fa6c8c34
Des Dulianto <desdulianto@gmail.com>
2019-10-04 21:26:44 +0700

    add DateRange isOverlapsWith function

--- src/main/kotlin/com/github/mkdika/jenerickt/function/JenericFun.kt
+++ src/main/kotlin/com/github/mkdika/jenerickt/function/JenericFun.kt
@@ -1,2 +1,16 @@
 package com.github.mkdika.jenerickt.function
 
+import java.lang.IllegalArgumentException
+import java.time.LocalDateTime
+
+class DateRange(from: LocalDateTime, to: LocalDateTime) {
+    val from = from
+    val to = to
+    init {
+        if (from > to) {
+            throw IllegalArgumentException("from date must be <= to date")
+        }
+    }
+    fun isOverlapsWith(date: DateRange): Boolean =
+        (this.from >= date.from && this.from <= date.to) || (this.to >= date.from && this.to <= date.to)
+}
--- src/test/kotlin/com/github/mkdika/jenerickt/function/JenericFunTests.kt
+++ src/test/kotlin/com/github/mkdika/jenerickt/function/JenericFunTests.kt
@@ -1,2 +1,40 @@
 package com.github.mkdika.jenerickt.function
 
+import junitparams.JUnitParamsRunner
+import junitparams.Parameters
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.time.LocalDateTime
+import java.util.*
+
+@RunWith(JUnitParamsRunner::class)
+class JenericFunTests {
+    fun dateRangeOverlapsParams(): Array<Any> = arrayOf(
+        arrayOf(DateRange(LocalDateTime.of(2018,1, 9, 10, 30, 45),
+            LocalDateTime.of(2018, 1, 10, 10, 30, 45)),
+            DateRange(LocalDateTime.of(2018, 1, 10, 10, 30, 45),
+                LocalDateTime.of(2018, 1, 20, 10, 30, 45)),
+            true)
+    )
+
+    @Test
+    @Parameters(method="dateRangeOverlapsParams")
+    fun `exec isOverlapsWith should return expected value`(range1: DateRange, range2: DateRange, expected: Boolean) {
+        val actual = range1.isOverlapsWith(range2)
+
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `exec initialization should throws IllegalArgumentException`() {
+        assertThatIllegalArgumentException().isThrownBy {
+            DateRange(
+                LocalDateTime.of(2018, 1, 10, 10, 30, 45),
+                LocalDateTime.of(2018, 1, 9, 10, 30, 45)
+            )
+        }
+    }
+}
+
