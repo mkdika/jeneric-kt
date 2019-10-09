842a56b7adbf6a63fabef4f44fd76934cd305587
Des Dulianto <desdulianto@gmail.com>
2019-10-10 00:00:10 +0700

    add chunkList extension function

--- src/main/kotlin/com/github/mkdika/jenerickt/function/GenericFun.kt
+++ src/main/kotlin/com/github/mkdika/jenerickt/function/GenericFun.kt
@@ -1,2 +1,22 @@
 package com.github.mkdika.jenerickt.function
 
+fun <T> List<T>.chunkList(chunkSize: Int): List<List<T>> =
+    when {
+        chunkSize <= 0 -> listOf(this)
+        chunkSize >= this.size -> listOf(this)
+        else -> {
+            val chunks = mutableListOf<List<T>>()
+            val chunkCount = this.size/chunkSize
+            val leftOver = this.size - (chunkCount * chunkSize)
+            for (i in 0 until chunkCount) {
+                val from = i * chunkSize
+                val to = if (from + chunkSize > this.size) this.size else from + chunkSize
+                val items = this.subList(from, to)
+                chunks.add(items)
+            }
+            if (leftOver > 0) {
+                chunks.add(this.subList(this.size-leftOver, this.size))
+            }
+            chunks.toList()
+        }
+    }
